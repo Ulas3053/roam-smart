9cfd20acdd6efe9e76f260b1692b65fc9053e9c1
Dheeraj Sanji <dheerajsanji123@gmail.com>
2025-07-29 10:48:56 +0530

    Custom-Itinery
    .enter place loaction budget itll give itinery

--- src/main/java/com/jsp/roam_smart/config/SecurityConfig.java
+++ src/main/java/com/jsp/roam_smart/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                     "/webjars/**",
                     "/api/auth/**",
                     "/error",
-                    "/api/place-search/explore"
+                    "/api/place-search/**"
                 ).permitAll()
                 .anyRequest().authenticated()
             )
